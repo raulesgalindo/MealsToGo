2e6bbd17604971fe4b99931309c676497c2e9b79
raulesgalindo <raulgalindorosales@gmail.com>
2022-11-15 11:41:30 -0600

    Adding spacer component

--- src/features/restaurants/components/restaurant-info-card.js
+++ src/features/restaurants/components/restaurant-info-card.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
-import { Text, View, Image } from "react-native";
 import { SvgXml } from "react-native-svg";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
+
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
@@ -18,20 +19,20 @@ const RestaurantCardCover = styled(Card.Cover)`
 
 const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fonts.caption};
-  color: ${(props) => props.theme.colors.ui.primary};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
+
 const Title = styled(Text)`
   font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
 `;
 
-const Info = styled(View)`
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const Rating = styled(View)`
+const Rating = styled.View`
   flex-direction: row;
   padding-top: ${(props) => props.theme.space[2]};
   padding-bottom: ${(props) => props.theme.space[2]};
@@ -41,6 +42,7 @@ const Section = styled.View`
   flex-direction: row;
   align-items: center;
 `;
+
 const SectionEnd = styled.View`
   flex: 1;
   flex-direction: row;
@@ -57,7 +59,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -69,8 +71,8 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Title>{name}</Title>
         <Section>
           <Rating>
-            {ratingArray.map ( () => (
-              <SvgXml xml={star} width={20} height={20}/>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
             ))}
           </Rating>
           <SectionEnd>
