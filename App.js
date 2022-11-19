d9648f4740d2b7f55097f384c95d162ffa2ee93a
Raul Galindo <rgalindo@ohmycard.com>
2022-11-18 19:02:14 -0600

    Adding icons

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
 
-import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
-import { useFonts as useLato, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import {
+  useFonts as useLato,
+  Lato_400Regular,
+  Lato_700Bold,
+} from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infraestructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
@@ -14,11 +22,27 @@ import { SafeArea } from "./src/components/utils/safe-area.component";
 
 const Tab = createBottomTabNavigator();
 
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
 const Settings = () => (
   <SafeArea>
     <Text>Settings</Text>
   </SafeArea>
 );
+
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
 const Map = () => (
   <SafeArea>
     <Text>Map</Text>
@@ -26,7 +50,6 @@ const Map = () => (
 );
 
 export default function App() {
-
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
   });
@@ -36,33 +59,39 @@ export default function App() {
     Lato_700Bold,
   });
 
-  if(!oswaldLoaded || !latoLoaded) {
+  if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
 
   function HomeScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Home!</Text>
       </View>
     );
   }
-  
+
   function SettingsScreen() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Settings!</Text>
       </View>
     );
   }
-  
+
   const Tab = createBottomTabNavigator();
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={createScreenOptions}
+            tabBarOptions={{
+              activeTintColor: "tomato",
+              inactiveTintColor: "gray",
+            }}
+          >
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
